f7b2137dcb2d603e3caf28220a064ced9caebaf7
Nathan Ilsley <nathan.ilsley@gmail.com>
2025-03-25 18:01:35 +0000

    Add position clamping

--- src/main.rs
+++ src/main.rs
@@ -14,28 +14,37 @@ fn window_conf() -> Conf{
     }
 }
 
+
 #[macroquad::main(window_conf)]
 async fn main() {
+    const MOVEMENT_SPEED: f32 = 200.0;
+    const CIRCLE_RADIUS: f32 = 16.0;
+    
     let mut x = screen_width() / 2.0;
     let mut y = screen_height() / 2.0;
 
     loop {
         clear_background(DARKBLUE);
 
+        let delta_time = get_frame_time();
+
         if is_key_down(KeyCode::D) {
-            x += 5.0;
+            x += MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::A) {
-            x -= 5.0;
+            x -= MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::W) {
-            y -= 5.0;
+            y -= MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::S) {
-            y += 5.0;
+            y += MOVEMENT_SPEED * delta_time;
         }
 
-        draw_circle(x, y, 16.0, YELLOW);
+        x = clamp(x, CIRCLE_RADIUS, screen_width() - CIRCLE_RADIUS);
+        y = clamp(y, CIRCLE_RADIUS, screen_height() - CIRCLE_RADIUS);
+
+        draw_circle(x, y, CIRCLE_RADIUS, YELLOW);
 
         next_frame().await;
     }
