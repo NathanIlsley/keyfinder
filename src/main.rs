9df457d3d4427fb61467ad9ef1700561fd39932a
Nathan Ilsley <nathan.ilsley@gmail.com>
2025-03-25 12:00:19 +0000

    Testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number!");
+
+    let mut guess = String::new();
+    guess = input("Please input your guess: ");
+
+    println!("You guessed: {}", guess);
+}
+
+fn input(message: &str) -> String {
+    print!("{}", message);
+    io::stdout().flush().expect("flush failed!");
+
+    let mut inp: String = String::new();
+
+    io::stdin()
+        .read_line(&mut inp)
+        .expect("Failed to read line!");
+
+    return inp;
 }
