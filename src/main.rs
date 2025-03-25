b48724288c4f5532d6b647f76444d898bbac56d2
Nathan Ilsley <nathan.ilsley@gmail.com>
2025-03-25 18:42:11 +0000

    Add gravity

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::f32::INFINITY;
 use macroquad::{miniquad::conf::Platform, prelude::*};
 
 fn window_conf() -> Conf{
@@ -17,11 +18,15 @@ fn window_conf() -> Conf{
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    const MOVEMENT_SPEED: f32 = 200.0;
+    const X_MOVEMENT_SPEED: f32 = 200.0;
+    const Y_MOVEMENT_SPEED: f32 = 800.0;
+    const JUMP_SPEED: f32 = 600.0;
+    const GRAVITY: f32 = 1000.0;
     const CIRCLE_RADIUS: f32 = 16.0;
-    
+
     let mut x = screen_width() / 2.0;
     let mut y = screen_height() / 2.0;
+    let mut ydot = 0.0;
 
     loop {
         clear_background(DARKBLUE);
@@ -29,20 +34,27 @@ async fn main() {
         let delta_time = get_frame_time();
 
         if is_key_down(KeyCode::D) {
-            x += MOVEMENT_SPEED * delta_time;
+            x += X_MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::A) {
-            x -= MOVEMENT_SPEED * delta_time;
+            x -= X_MOVEMENT_SPEED * delta_time;
         }
-        if is_key_down(KeyCode::W) {
-            y -= MOVEMENT_SPEED * delta_time;
-        }
-        if is_key_down(KeyCode::S) {
-            y += MOVEMENT_SPEED * delta_time;
+        if is_key_down(KeyCode::W) && y == screen_height() - CIRCLE_RADIUS {
+            ydot = -JUMP_SPEED;
         }
 
+        ydot += GRAVITY * delta_time;
+        ydot = clamp(ydot, -Y_MOVEMENT_SPEED, INFINITY);
+
+        y += ydot * delta_time;
+
         x = clamp(x, CIRCLE_RADIUS, screen_width() - CIRCLE_RADIUS);
-        y = clamp(y, CIRCLE_RADIUS, screen_height() - CIRCLE_RADIUS);
+        let yclamp = clamp(y, CIRCLE_RADIUS, screen_height() - CIRCLE_RADIUS);
+
+        if yclamp != y {
+            ydot = 0.0;
+        }
+        y = yclamp;
 
         draw_circle(x, y, CIRCLE_RADIUS, YELLOW);
 
