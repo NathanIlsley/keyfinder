b8e5bf622a6a54aee4fed93bb841aea131d6cb6b
Nathan Ilsley <nathan.ilsley@gmail.com>
2025-03-25 17:21:24 +0000

    Set window config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
-use macroquad::prelude::*;
+use macroquad::{miniquad::conf::Platform, prelude::*};
 
-#[macroquad::main("Key Finder")]
+fn window_conf() -> Conf{
+    Conf {
+        window_title: String::from("Key Finder"),
+        window_width: 640,
+        window_height: 400,
+        high_dpi: false,
+        fullscreen: false,
+        sample_count: 1,
+        window_resizable: false,
+        icon: None,
+        platform: Platform::default(),
+    }
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
     loop {
         clear_background(DARKBLUE);
