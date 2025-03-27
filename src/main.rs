199651d10e0c4fe77b02f7aca8091a37340d6ebd
Nathan Ilsley <nathan.ilsley@gmail.com>
2025-03-27 18:55:59 +0000

    Add scrolling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use macroquad::{miniquad::conf::Platform, prelude::*};
+use std::{f32::INFINITY, thread::sleep, time::Duration};
+
+use macroquad::{miniquad::conf::Platform, prelude::{coroutines::wait_seconds, *}, telemetry::frame};
 
 fn window_conf() -> Conf{
     Conf {
@@ -94,9 +96,11 @@ impl Vector2 {
 struct Player {
     position: Vector2,
     velocity: Vector2,
-    platforms: Vec<Plat>,
+    // platforms: Vec<Plat>,
     grounded: bool,
     jump_time: f32,
+
+    scroll: f32,
 }
 
 impl Player {
@@ -109,19 +113,21 @@ impl Player {
 
     fn new() -> Self {
         Self {
-            position: Vector2 { x: screen_width() / 2.0, y: screen_height() / 2.0 },
+            position: Vector2 { x: screen_width() / 2.0, y: screen_height() * 0.75 },
             velocity: Vector2 { x: 0.0, y: 0.0 },
-            platforms: vec![],
+            // platforms: vec![],
             grounded: false,
             jump_time: 0.0,
+
+            scroll: 0.0,
         }
     }
 
-    fn set_plats(&mut self, plats: Vec<Plat>) {
-        self.platforms = plats;
-    }
+    // fn set_plats(&mut self, plats: Vec<Plat>) {
+    //     self.platforms = plats;
+    // }
     
-    fn update(&mut self) {
+    fn update(&mut self, platforms: Vec<&Plat>) {
         let delta_time = get_frame_time();
 
         // Inputs
@@ -132,8 +138,6 @@ impl Player {
         };
 
         self.velocity.x = clamp(self.velocity.x, -Player::X_MOVEMENT_SPEED, Player::X_MOVEMENT_SPEED);
-
-        // self.jump_time += delta_time;
 
         if is_key_down(KeyCode::W) && self.jump_time <= 0.2 {
             self.velocity.y = -Player::JUMP_SPEED;
@@ -154,7 +158,7 @@ impl Player {
         // Check Collisions
         self.grounded = false;
 
-        if self.position.clamp(Player::CIRCLE_RADIUS, screen_width() - Player::CIRCLE_RADIUS, Player::CIRCLE_RADIUS, screen_height() - Player::CIRCLE_RADIUS).y != 0.0 {
+        if self.position.clamp(Player::CIRCLE_RADIUS, screen_width() - Player::CIRCLE_RADIUS, -INFINITY, screen_height() - Player::CIRCLE_RADIUS).y != 0.0 {
             self.velocity.y = 0.0;
             
             if self.position.y == screen_height() - Player::CIRCLE_RADIUS {
@@ -165,7 +169,7 @@ impl Player {
         let mut dist: Vector2;
         let mut overlap: Vector2;
 
-        for plat in &self.platforms {
+        for plat in platforms {
             dist = self.position.sub(&plat.position);
             if dist.x.abs() < plat.dimensions.x / 2.0 + Player::CIRCLE_RADIUS && dist.y.abs() < plat.dimensions.y / 2.0 + Player::CIRCLE_RADIUS {
                 overlap = Vector2::new(
@@ -184,8 +188,16 @@ impl Player {
         }
 
         // Draw on screen
-        draw_circle(self.position.x, self.position.y, Player::CIRCLE_RADIUS, YELLOW);
+        draw_circle(self.position.x, self.position.y + self.scroll, Player::CIRCLE_RADIUS, YELLOW);
 
+    }
+
+    fn set_scroll(&mut self, scroll: f32) {
+        self.scroll = scroll;
+    }
+
+    fn adjusted_y(&self) -> f32 {
+        self.position.y + self.scroll
     }
 }
 
@@ -193,19 +205,56 @@ impl Player {
 struct Plat {
     position: Vector2,
     dimensions: Vector2,
+
+    scroll: f32,
 }
 
 impl Plat {
     fn new(x: f32, y: f32, w: f32, h: f32) -> Self {
-        Self { position: Vector2::new(x, y) , dimensions: Vector2::new(w, h)}
+        Self { position: Vector2::new(x, y) , dimensions: Vector2::new(w, h), scroll: 0.0 }
     }
 
     fn update(&mut self) {
         // Draw on screen
-        draw_rectangle(self.position.x - self.dimensions.x / 2.0, self.position.y - self.dimensions.y / 2.0, self.dimensions.x, self.dimensions.y, WHITE);
+        let adjusted_y = self.position.y + self.scroll; // Adjust y for scroll
+
+        if adjusted_y > -self.dimensions.y && adjusted_y < screen_height() {
+            draw_rectangle(self.position.x - self.dimensions.x / 2.0, adjusted_y - self.dimensions.y / 2.0, self.dimensions.x, self.dimensions.y, WHITE);
+        }
+    }
+    
+    fn set_scroll(&mut self, scroll: f32) {
+        self.scroll = scroll;
     }
 }
 
+struct Screen {
+    // player: Player,
+    // platforms: Vec<Plat>,
+    scroll_pos: f32,
+}
+
+impl Screen {
+
+    fn new() -> Self {
+        // Screen { player: player, platforms: vec![], scroll_pos: 0.0 }
+        Screen { scroll_pos: 0.0 }
+    }
+
+    // fn set_plats(&mut self, plats: Vec<Plat>) {
+    //     self.platforms = plats;
+    // }
+
+    fn update(&mut self, player: &mut Player, platforms: Vec<&mut Plat>) {
+
+        self.scroll_pos += if self.scroll_pos <= 0.0 && player.adjusted_y() > screen_height() * 0.6 {0.0} else {screen_height() * 0.6 - player.adjusted_y()};
+
+        player.set_scroll(self.scroll_pos);
+        for plat in platforms {
+            plat.set_scroll(self.scroll_pos);
+        }
+    }
+}
 
 #[macroquad::main(window_conf)]
 async fn main() {
@@ -213,20 +262,34 @@ async fn main() {
     let mut platform1 = Plat::new(200.0, 300.0, 300.0, 25.0);
     let mut platform2 = Plat::new(500.0, 100.0, 300.0, 25.0);
     let mut platform3 = Plat::new(650.0, 400.0, 300.0, 25.0);
-    let platforms = vec![platform1, platform2, platform3];
+    let mut platform4 = Plat::new(200.0, -100.0, 300.0, 25.0);
+    let mut screen = Screen::new();
 
-    player.set_plats(platforms);
+    // player.set_plats(platforms);
+    // screen.set_plats(platforms);    
 
     loop {
         clear_background(DARKBLUE);
-
-        player.update();
+        
+        player.update(vec![&platform1, &platform2, &platform3, &platform4]);
         platform1.update();
         platform2.update();
         platform3.update();
+        platform4.update();
+
+        screen.update(&mut player, vec![&mut platform1, &mut platform2, &mut platform3, &mut platform4]);
 
         // println!("{}", get_fps());
-
+        
+        frame_control();
         next_frame().await;
     }
 }
+
+#[cfg(target_os="windows")]
+fn frame_control() {
+    sleep(Duration::from_millis(1));
+}
+
+#[cfg(not(target_os="windows"))]
+fn frame_control() {}
